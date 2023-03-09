cdb6846861f1c335f2a98a3cb560a5479e6a5d35
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-08 21:30:30 -0800

    complete arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = [ 'Milo', 'Otis', 'Garfield' ];
+
+function destructivelyAppendCat(cat)
+{
+   cats.push(cat)
+}
+
+function destructivelyPrependCat(cat)
+{
+   cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat()
+{
+   cats.pop()
+}
+
+function destructivelyRemoveFirstCat()
+{
+   cats.shift()
+}
+
+function appendCat(cat)
+{
+   const copycat = [...cats]
+   copycat.push(cat)
+
+   return copycat
+}
+
+function prependCat(cat)
+{
+   const copycat = [...cats]
+   copycat.unshift(cat)
+
+   return copycat
+}
+
+function removeLastCat()
+{
+   const copycat = [...cats]
+   copycat.pop()
+
+   return copycat
+}
+
+function removeFirstCat()
+{
+   const copycat = [...cats]
+   copycat.shift()
+
+   return copycat
+}
